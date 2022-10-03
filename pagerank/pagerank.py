79bcf63bc54015a4d978319ed9b20bdda7458670
PrasannaIITM <prasanna.bartakke@gmail.com>
2022-10-03 17:02:46 +0530

    cleaning pagerank

--- pagerank/pagerank.py
+++
@@ -1,6 +0,0 @@
-import networkx as nx
-
-G = nx.DiGraph()
-edges = [(0, 1), (0, 2), (1, 2), (1, 3), (1, 4), (2, 3), (3, 1), (3, 4), (4, 3)]
-pr = nx.pagerank(G)
-print(pr)
--- pagerank/pr_pull.cpp
+++ pagerank/pr_pull.cpp
@@ -2,43 +2,39 @@
 #include <iostream>
 #include <fstream>
 #define DEBUG 0
-#define NUM_THREADS 4
 
 using namespace sycl;
 
-int main()
+int main(int argc, char **argv)
 {
 
     std::chrono::steady_clock::time_point tic_0 = std::chrono::steady_clock::now();
-    std::string name = "simple";
-    std::cout << "Processing " << name << std::endl;
-    // The default device selector will select the most performant device.
+    std::ofstream logfile;
+
+    std::string name = argv[1];
+    int NUM_THREADS = atoi(argv[2]);
+    std::string NUM_THREADS_STR = std::to_string(NUM_THREADS);
+
+    logfile.open("pagerank/output/" + name + "_pr_pull_time_" + NUM_THREADS_STR + ".txt");
+
+    logfile << "Processing " << name << std::endl;
+
     default_selector d_selector;
     queue Q(d_selector);
-    std::cout << "Selected device: " << Q.get_device().get_info<info::device::name>() << "\n";
+    logfile << "Selected device: " << Q.get_device().get_info<info::device::name>() << std::endl;
+    logfile << "Number of parallel work items: " << NUM_THREADS << std::endl;
 
     std::chrono::steady_clock::time_point tic = std::chrono::steady_clock::now();
     std::vector<int> V, I, E, RE, RI;
-    load_from_file("input/" + name + "/V", V);
-    load_from_file("input/" + name + "/I", I);
-    load_from_file("input/" + name + "/E", E);
-    load_from_file("input/" + name + "/RE", RE);
-    load_from_file("input/" + name + "/RI", RI);
+    load_from_file("csr_graphs/" + name + "/V", V);
+    load_from_file("csr_graphs/" + name + "/I", I);
+    load_from_file("csr_graphs/" + name + "/RE", RE);
+    load_from_file("csr_graphs/" + name + "/RI", RI);
     std::chrono::steady_clock::time_point toc = std::chrono::steady_clock::now();
-    std::cout << "Time to load data from files: " << std::chrono::duration_cast<std::chrono::microseconds>(toc - tic).count() << "[µs]" << std::endl;
+    logfile << "Time to load data from files: " << std::chrono::duration_cast<std::chrono::microseconds>(toc - tic).count() << "[µs]" << std::endl;
 
-    if (DEBUG)
-    {
-        std::cout << "Node: ";
-        print_vector(V);
-        std::cout << "Offset: ";
-        print_vector(I);
-        std::cout << "Edge: ";
-        print_vector(E);
-    }
     int *dev_V = malloc_device<int>(V.size(), Q);
     int *dev_I = malloc_device<int>(I.size(), Q);
-    int *dev_E = malloc_device<int>(E.size(), Q);
     int *dev_RE = malloc_device<int>(RE.size(), Q);
     int *dev_RI = malloc_device<int>(RI.size(), Q);
 
@@ -51,9 +47,6 @@ int main()
 
     Q.submit([&](handler &h)
              { h.memcpy(dev_I, &I[0], I.size() * sizeof(int)); });
-
-    Q.submit([&](handler &h)
-             { h.memcpy(dev_E, &E[0], E.size() * sizeof(int)); });
 
     Q.submit([&](handler &h)
              { h.memcpy(dev_RE, &RE[0], RE.size() * sizeof(int)); });
@@ -80,18 +73,16 @@ int main()
                                   } }); });
     Q.wait();
 
+    tic = std::chrono::steady_clock::now();
+    logfile << "Starting Pagerank..." << std::endl;
+    float *diff = malloc_shared<float>(1, Q);
+    int iterCount = 0;
+    do
     {
-
-        tic = std::chrono::steady_clock::now();
-        std::cout << "Starting Pagerank..." << std::endl;
-        float *diff = malloc_shared<float>(1, Q);
-        int iterCount = 0;
-        do
-        {
-            Q.submit([&](handler &h)
-                     { h.parallel_for(
-                           NUM_THREADS, [=](id<1> i)
-                           {
+        Q.submit([&](handler &h)
+                 { h.parallel_for(
+                       NUM_THREADS, [=](id<1> i)
+                       {
                                for (; i < N; i += stride)
                                {
                                    float sum = 0;
@@ -105,44 +96,43 @@ int main()
                                    atomic_data += (float)val - dev_pagerank[i];
                                    dev_pagerank_i[i] = val;
                                } }); })
-                .wait();
+            .wait();
 
-            Q.submit([&](handler &h)
-                     { h.parallel_for(
-                           NUM_THREADS, [=](id<1> i)
-                           {
+        Q.submit([&](handler &h)
+                 { h.parallel_for(
+                       NUM_THREADS, [=](id<1> i)
+                       {
                                 for (; i < N; i += stride)
                                 {
                                     dev_pagerank[i] = dev_pagerank_i[i];
                                 } }); })
-                .wait();
-            iterCount += 1;
-        } while ((*diff > beta) && (iterCount < maxIter));
+            .wait();
+        iterCount += 1;
+    } while ((*diff > beta) && (iterCount < maxIter));
 
-        toc = std::chrono::steady_clock::now();
-        std::cout << "Time to run Pagerank: " << std::chrono::duration_cast<std::chrono::microseconds>(toc - tic).count() << "[µs]" << std::endl;
-    }
+    toc = std::chrono::steady_clock::now();
+    logfile << "Time to run Pagerank: " << std::chrono::duration_cast<std::chrono::microseconds>(toc - tic).count() << "[µs]" << std::endl;
+
     tic = std::chrono::steady_clock::now();
-    std::ofstream myfile;
+    std::ofstream resultfile;
     int num_covered = 0;
 
     Q.submit([&](handler &h)
              { h.memcpy(&pagerank[0], dev_pagerank, N * sizeof(float)); })
         .wait();
 
-    std::string NUM_THREADS_STR = std::to_string(NUM_THREADS);
-    myfile.open("output/" + name + "/pagerank_v1_result_" + NUM_THREADS_STR + ".txt");
+    resultfile.open("pagerank/output/" + name + "_pr_pull_result_" + NUM_THREADS_STR + ".txt");
 
     for (int i = 0; i < N; i++)
     {
-        myfile << i << " " << pagerank[i] << std::endl;
+        resultfile << i << " " << pagerank[i] << std::endl;
     }
-    myfile.close();
+    resultfile.close();
     toc = std::chrono::steady_clock::now();
-    std::cout << "Time to write data to file: " << std::chrono::duration_cast<std::chrono::microseconds>(toc - tic).count() << "[µs]" << std::endl;
+    logfile << "Time to write data to file: " << std::chrono::duration_cast<std::chrono::microseconds>(toc - tic).count() << "[µs]" << std::endl;
 
     std::chrono::steady_clock::time_point toc_0 = std::chrono::steady_clock::now();
-    std::cout << "Total time taken: " << std::chrono::duration_cast<std::chrono::microseconds>(toc_0 - tic_0).count() << "[µs]" << std::endl;
+    logfile << "Total time taken: " << std::chrono::duration_cast<std::chrono::microseconds>(toc_0 - tic_0).count() << "[µs]" << std::endl;
 
     return 0;
 }
